6a4a7427143d07fe2302ad2fa3a935c575ece27f
Lucas Campos <lucascampos.ads@gmail.com>
2021-07-30 13:35:14 -0300

    feat: Started server and config dev:server script with ts-node-dev

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World, Gazin tech.' });
+  return response.json({ message: 'Hello World, Gazin tech!' });
 });
 
 app.listen(3333, () => {
