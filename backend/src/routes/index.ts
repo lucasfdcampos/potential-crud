e93c2106850d459ac57a3d6c3eda3f7cec816a15
Lucas Campos <lucascampos.ads@gmail.com>
2021-07-30 20:38:13 -0300

    feat: Add user route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+
 import developersRouter from './developers.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/developers', developersRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
