fe0fc338f82d75ec958d2d0820d572da943de595
Lucas Campos <lucascampos.ads@gmail.com>
2021-07-30 13:31:16 -0300

    feat: Config tsconfig rootDir and outDir paths

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World, Gazin tech.' });
+});
+
+app.listen(3333, () => {
+  console.log('🚀 [Gazin tech] Server started on port 3333.');
+});
