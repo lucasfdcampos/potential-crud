ccf9633b34a5583a6ac04d6eb67b84bda488d713
Lucas Campos <lucascampos.ads@gmail.com>
2021-07-30 19:27:29 -0300

    feat: Add initial repositories with typeorm

--- backend/src/repositories/DevelopersRepository.ts
+++ backend/src/repositories/DevelopersRepository.ts
@@ -1,37 +1,8 @@
+import { EntityRepository, Repository } from 'typeorm';
+
 import Developer from '../models/Developer';
 
-interface CreateDeveloperDTO {
-  name: string;
-  sex: string;
-  age: number;
-  hobby: string;
-  birthdate: Date;
-}
-
-class DevelopersRepository {
-  private developers: Developer[];
-
-  constructor() {
-    this.developers = [];
-  }
-
-  public all(): Developer[] {
-    return this.developers;
-  }
-
-  public create({
-    name,
-    sex,
-    age,
-    hobby,
-    birthdate,
-  }: CreateDeveloperDTO): Developer {
-    const developer = new Developer({ name, sex, age, hobby, birthdate });
-
-    this.developers.push(developer);
-
-    return developer;
-  }
-}
+@EntityRepository(Developer)
+class DevelopersRepository extends Repository<Developer> {}
 
 export default DevelopersRepository;
