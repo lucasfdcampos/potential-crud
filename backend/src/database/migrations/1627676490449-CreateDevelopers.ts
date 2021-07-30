01f09b659385cec0a61e4d7497200ba14e7e0261
Lucas Campos <lucascampos.ads@gmail.com>
2021-07-30 19:07:35 -0300

    feat: Add Migrations of creation tables

---
+++ backend/src/database/migrations/1627676490449-CreateDevelopers.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateDevelopers1627676490449 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'developers',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'sex',
+            type: 'varchar',
+          },
+          {
+            name: 'age',
+            type: 'integer',
+          },
+          {
+            name: 'hobby',
+            type: 'varchar',
+          },
+          {
+            name: 'birthdate',
+            type: 'timestamp',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('developers');
+  }
+}
